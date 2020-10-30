d3a1a32d43728a83792a24df3d841c66cb9d90e7
Coldani3 <p110117069@gmail.com>
2020-10-30 23:13:26 +0000

    Stylised with Skeleton

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,8 @@
 <template>
   <DynamicList listName="TODO-List:" />
-  <!-- <img alt="Vue logo" src="./assets/logo.png" /> -->
-  <!-- <HelloWorld msg="Welcome to Your Vue.js App" /> -->
 </template>
 
 <script>
-//import HelloWorld from "./components/HelloWorld.vue";
 import DynamicList from "./components/VueTest.vue";
 
 export default {
--- src/components/VueTest.vue
+++ src/components/VueTest.vue
@@ -1,13 +1,14 @@
 <template>
     <div id="dynamicList">
         <h1>{{listName}}</h1>
-        <div id="list" style="border-style: solid; border-width: 1px; border-color: black; height: 400px; width: 300px; overflow: auto;">
+        <div id="list" style="border-style: solid; border-width: 1px; border-color: black; height: 400px; width: 400px; overflow: auto; margin-bottom: 5px;">
             <ul>
                 <li v-for="item in currentList" v-bind:key="item">{{item}}</li>    
             </ul>
         </div>
-        <input type="text" v-model="this.currentText">
-        <button v-on:click="addToList">Add</button>
+        <input type="text" v-model="this.currentText" style="width:400px">
+        <br/>
+        <button v-on:click="addToList" style="margin-right:15px">Add</button>
         <button v-on:click="clearList">Clear</button>
     </div>
 </template>
