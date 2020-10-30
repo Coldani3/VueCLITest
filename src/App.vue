743d74a6dc728f6eca24d92978db61f0c3ed53c2
Coldani3 <p110117069@gmail.com>
2020-10-30 23:01:39 +0000

    Sample site

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,17 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js App" />
+  <DynamicList listName="TODO-List:" />
+  <!-- <img alt="Vue logo" src="./assets/logo.png" /> -->
+  <!-- <HelloWorld msg="Welcome to Your Vue.js App" /> -->
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+//import HelloWorld from "./components/HelloWorld.vue";
+import DynamicList from "./components/VueTest.vue";
 
 export default {
   name: "App",
   components: {
-    HelloWorld
+    DynamicList
   }
 };
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/VueTest.vue
@@ -0,0 +1,43 @@
+<template>
+    <div id="dynamicList">
+        <h1>{{listName}}</h1>
+        <div id="list" style="border-style: solid; border-width: 1px; border-color: black; height: 400px; width: 300px; overflow: auto;">
+            <ul>
+                <li v-for="item in currentList" v-bind:key="item">{{item}}</li>    
+            </ul>
+        </div>
+        <input type="text" v-model="this.currentText">
+        <button v-on:click="addToList">Add</button>
+        <button v-on:click="clearList">Clear</button>
+    </div>
+</template>
+
+<script>
+export default {
+  el: "#dynamicList",
+  name: "DynamicList",
+  data()
+  {
+      return {
+          currentText: "",
+          currentList: 
+          [
+
+          ]
+      }
+  },
+  props: {
+    listName: String
+  },
+  methods: {
+    addToList()
+    {
+        this.currentList.push(this.currentText);
+    },
+    clearList()
+    {
+        this.currentList = [];
+    }
+  }
+};
+</script>
